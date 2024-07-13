ad82135e9c15f4e8af33556c4567a9d3b8c63c28
TrulyBright <trulybright@yonsei.ac.kr>
2024-07-13 16:06:25 +0900

    format & lint

--- api/src/code.ts
+++ api/src/code.ts
@@ -1,4 +1,8 @@
 export type Code = string & { __brand: "Code" }
 export const isCode = (s: unknown): s is Code =>
     typeof s === "string" && /^[A-Z0-9]{8}$/.test(s)
-export const randomCode = (): Code => Math.random().toString(26 + 10).slice(2, 10).toUpperCase() as Code
+export const randomCode = (): Code =>
+    Math.random()
+        .toString(26 + 10)
+        .slice(2, 10)
+        .toUpperCase() as Code
--- server/src/hub.ts
+++ server/src/hub.ts
@@ -1,4 +1,4 @@
-import { randomCode, type Code } from "@dartagnan/api/code"
+import { type Code, randomCode } from "@dartagnan/api/code"
 import { NoSuchRoom, RoomFull } from "@dartagnan/api/error"
 import { Room } from "#room"
 import type { User } from "#user"
@@ -12,29 +12,21 @@ export class Hub {
      */
     addUser(u: User, code: Code | null) {
         let r: Room
-        if (code === null)
-            r = new Room(randomCode())
-        else if (!this.rooms[code])
-            throw new NoSuchRoom(code)
-        else if (this.rooms[code].full)
-            throw new RoomFull()
-        else
-            r = this.rooms[code]
+        if (code === null) r = new Room(randomCode())
+        else if (!this.rooms[code]) throw new NoSuchRoom(code)
+        else if (this.rooms[code].full) throw new RoomFull()
+        else r = this.rooms[code]
         r.addUser(u)
         u.setRoom(r)
-        if (r.users.length === 1)
-            r.setHost(u)
+        if (r.users.length === 1) r.setHost(u)
         this.rooms[r.code] = r
     }
     removeUser(u: User) {
         const r = u.room
-        if (!r)
-            return
+        if (!r) return
         r.removeUser(u)
         u.unsetRoom()
-        if (r.empty)
-            delete this.rooms[r.code]
-        else if (r.host === u)
-            r.setHost(r.users[0])
+        if (r.empty) delete this.rooms[r.code]
+        else if (r.host === u) r.setHost(r.users[0])
     }
 }
