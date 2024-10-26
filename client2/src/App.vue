003b6e2eeddfaee531db8d2fbbf92c01d083e055
TrulyBright <trulybright@yonsei.ac.kr>
2024-10-26 23:40:55 +0900

    remove the legacy of refactoring

--- client2/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Title from '@/components/Title.vue'
-</script>
-<template>
-  <div class="mb-5">
-    <Title />
-  </div>
-  <RouterView class="grow" />
-</template>
--- client2/src/components/Title.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h2 class="text-4xl text-center">달타냥</h2>
-    <p class="text-sm text-center">
-        <i class="font-serif">
-            Shoot to be the last one standing
-        </i>
-    </p>
-</template>
