ff93769f3eb8bc50f30c3c2507b037c4673335d6
TrulyBright <trulybright@yonsei.ac.kr>
2024-10-30 15:07:29 +0900

    use locale & color

--- client/src/views/RoomView.vue
+++ client/src/views/RoomView.vue
@@ -2,20 +2,29 @@
 import { computed, reactive, ref } from 'vue'
 import { type Drift } from "@dartagnan/api/drift"
 import { type Cmd } from "@dartagnan/api/cmd"
+import { Sharpshooter, type Card } from "@dartagnan/api/card"
+import locale, { type CardLocale } from '@/locale'
+import { colorOf } from '@/color'
 
 const WS = reactive(new WebSocket(`ws://${location.host}/game`))
 
-const drift = ref<Drift>(0)
 const message = ref<string>('')
+const drift = ref<Drift>(0)
 const driftText = computed(() => drift.value === 1 ? "증가" : drift.value === -1 ? "감소" : "유지")
 const driftColor = computed(() => drift.value === 1 ? "text-green-400" : drift.value === -1 ? "text-red-400" : "text-yellow-400")
+const card = ref<Card | null>(Sharpshooter)
+const cardColor = computed(() => {
+    if (!card.value) return undefined
+    return `text-${colorOf(card.value)}-400`
+})
+const cardLocale = computed<CardLocale>(() => card.value ? locale.ko.card[card.value.tag] : null)
 
 const sendCmd = (c: Cmd) => WS.send(JSON.stringify(c))
 
 const onMessageInputEnter = (e: KeyboardEvent) => {
     if (e.key === "Enter" && !e.isComposing) speak()
 }
-const onMessageInput = (e: InputEvent) => {
+const onMessageInput = (e: Event) => {
     message.value = (e.target as HTMLInputElement).value
 }
 const speak = () => {
@@ -34,7 +43,7 @@ const toggleDrift = () => {
         <div class="size-96 border-2 object-center">게임판</div>
         <div>
             <div class="flex flex-row">
-                <button type="button" class="grow text-xs break-keep" @click="toggleDrift">
+                <button type="button" class="w-50 text-xs break-keep" @click="toggleDrift">
                     <div>
                         <p>
                             명중률이 대체로 <strong :class="driftColor">{{ driftText }}</strong>
@@ -43,13 +52,24 @@ const toggleDrift = () => {
                         <p>누르면 경향세가 바뀝니다.</p>
                     </div>
                 </button>
-                <button type="button" disabled class="grow text-xs break-keep disabled:opacity-75 disabled:cursor-not-allowed">
-                    <p>누르면 <strong class="text-blue-400">보험</strong> 카드를 사용합니다.</p>
-                    <p><strong class="text-blue-400">보험</strong>: 10달러를 지불합니다. 사망 시 80달러를 얻습니다.</p>
+                <button type="button" disabled class="w-50 text-xs break-keep disabled:opacity-75 disabled:cursor-not-allowed">
+                    <div  v-if="card">
+                        <p>누르면 <strong :class="cardColor">{{ cardLocale.name }}</strong> 카드를 사용합니다.</p>
+                        <p><strong :class="cardColor">{{ cardLocale.name }}</strong>: {{ cardLocale.description }}</p>
+                    </div>
+                    <p v-else>카드가 없습니다. 누르면 카드를 뽑습니다.</p>
                 </button>
             </div>
             <div class="flex">
-                <input type="text" class="grow p-1 rounded-md" placeholder="여기에 할 말 입력..." :value="message" maxlength="80" @input="onMessageInput" @keydown="onMessageInputEnter" />
+                <input
+                    type="text"
+                    class="grow p-1 rounded-md"
+                    placeholder="여기에 할 말 입력..."
+                    :value="message"
+                    maxlength="80"
+                    @input="onMessageInput"
+                    @keydown="onMessageInputEnter"
+                />
                 <button type="button" class="p-1 rounded-md" @click="speak">전송</button>
             </div>
         </div>
